2a0e00fe5d9ff893856eeb883ebabcd931821ea3
HLJT <2696572468@qq.com>
2024-01-18 12:19:21 +0800

    init repo

---
+++ src/main/java/com/lt/pc_community/PcCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.lt.pc_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lt/pc_community/PcCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.pc_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
